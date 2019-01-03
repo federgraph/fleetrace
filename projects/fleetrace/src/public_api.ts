2873235ead9c9ff01a872bcdd8d19ce1278ca51d
Gustav Schubert <gschub@googlemail.com>
2019-01-03 16:09:08 +0100

    initial code for fleetrace library project

---
+++ projects/fleetrace/src/public_api.ts
@@ -0,0 +1,74 @@
+/*
+ * Public API Surface of fr
+ */
+
+export * from './fleetrace.module';
+
+export * from './bo/bo-base';
+export * from './bo/bo-main-params';
+export * from './bo/bo-manager';
+export * from './bo/bo-msg-base';
+export * from './bo/bo-msg-context';
+export * from './bo/bo-msg-list';
+export * from './bo/bo-msg-parser';
+export * from './bo/bo-msg-token';
+export * from './bo/bo-msg-tree';
+export * from './bo/bo-msg';
+export * from './bo/bo-params';
+
+export * from './calc/calc-ev';
+export * from './calc/calc-rs-01';
+export * from './calc/calc-rs-03';
+export * from './calc/calc-tp';
+export * from './calc/calc-uniqua-points';
+export * from './calc/penalty-isaf';
+export * from './calc/q-proxy';
+export * from './calc/status';
+export * from './calc/time';
+
+export * from './col/event/event-bo';
+export * from './col/event/event-enums';
+export * from './col/event/event-grid';
+export * from './col/event/event-race-entry';
+export * from './col/event/event-row-collection';
+
+export * from './col/name-value/name-value-grid';
+
+export * from './col/race/race-bo';
+export * from './col/race/race-col-prop';
+export * from './col/race/race-grid';
+export * from './col/race/race-node';
+export * from './col/race/race-row-collection-item';
+export * from './col/race/race-row-collection';
+export * from './col/race/time-point';
+
+export * from './col/stammdaten/stammdaten-bo';
+export * from './col/stammdaten/stammdaten-col-prop';
+export * from './col/stammdaten/stammdaten-fieldnames';
+export * from './col/stammdaten/stammdaten-grid';
+export * from './col/stammdaten/stammdaten-node';
+export * from './col/stammdaten/stammdaten-row-collection';
+export * from './col/stammdaten/stammdaten-row-collection-item';
+export * from './col/stammdaten/stammdaten-row-collection';
+
+export * from './fr/fr-bo';
+export * from './fr/fr-error-list';
+export * from './fr/fr-event-props';
+export * from './fr/fr-excel-export';
+export * from './fr/fr-excel-importer';
+export * from './fr/fr-ini-image';
+export * from './fr/fr-node-list';
+
+export * from './grid/col-captions';
+export * from './grid/col-grid';
+export * from './grid/col-item';
+export * from './grid/grid-cell-prop';
+export * from './grid/grid-cells';
+export * from './grid/grid-color';
+export * from './grid/grid-def';
+
+export * from './util/fb-classes';
+export * from './util/fb-color';
+export * from './util/fb-enumset';
+export * from './util/fb-props';
+export * from './util/fb-strings';
